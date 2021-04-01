4bbd8246c645f2f3a9517ad721d163d41dbecaaf
Simon Carryer <simon.carryer@xero.com>
2021-04-01 16:59:43 +1300

    human fitness function

--- main.py
+++ main.py
@@ -11,18 +11,26 @@ from breed.babies import make_babies
 from plants.plants import tree, daisy, cyclamen, foxglove
 
 
-if __name__ == "__main__":
-    set_up_screen()
-    # fill_background()
+def draw_functions(f1, f2):
     xs = range(80, 800, 200)
     ys = range(600, 0, -250)
     coords = [(x, y) for y in ys for x in xs]
-    for func, start in zip([daisy, foxglove], coords):
-        # clear_screen()
-        # fill_background()
+    funcs = list(make_babies(f1, f2))
+    for func, start in zip([f1, f2], coords):
         draw_from_function(func, start=start, incremental=True)
-    for func, start in zip(make_babies(daisy, foxglove), coords[2:]):
-        # clear_screen()
-        # fill_background()
+    for func, start in zip(funcs, coords[2:]):
         draw_from_function(func, start=start, incremental=True)
-    time.sleep(5)
+    return funcs
+
+
+if __name__ == "__main__":
+    f1 = daisy
+    f2 = foxglove
+    set_up_screen()
+    funcs = draw_functions(f1, f2)
+    print("choice 1")
+    f1 = funcs[int(input())]
+    print("choice 2")
+    f2 = funcs[int(input())]
+    turtle.getscreen().clear()
+    draw_functions(f1, f2)
